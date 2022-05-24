9459929ae8df1a90fb49e4122897fd6b2eefb6ba
Melki-244 <melkicavalcante244@gmail.com>
2022-05-24 14:03:46 -0300

    Conectando Com Banco de Dados | Conectando Com o Banco

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Melki-244/Go-Fundamentos_Web_Application
 
 go 1.18
+
+require github.com/lib/pq v1.10.6 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
+
+func conectaComOBancoDeDados() *sql.DB {
+  conexao := "user=postgres dbname=Alura-Loja password=33114665 host=localhost sslmode=disable"
+  db, err := sql.Open("postgres",conexao)
+
+  if err != nil {
+    panic(err.Error())
+  }
+  return db
+}
 
 var temp = template.Must(template.ParseGlob("templates/*.html")) 
 var porta = ":8080" 
@@ -17,6 +30,8 @@ type Produtos struct {
 }
 
 func main() {
+  db := conectaComOBancoDeDados()
+  defer db.Close()
   fmt.Println("Server Runing In localhost",porta)
   http.HandleFunc("/", index)
   http.ListenAndServe(porta, nil) 
