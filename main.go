6c8021342a6aaf8e22c6d92ebc46a6dbd27f563d
Melki-244 <melkicavalcante244@gmail.com>
2022-05-24 11:59:49 -0300

    Servidor e Struct de Produtos | Criando Struct de Produtos

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
 
 var temp = template.Must(template.ParseGlob("templates/*.html")) 
+var porta = ":8080" 
+
+type Produtos struct {
+  Nome string 
+  Descricao string
+  Preco float64
+  Quantidade int
+}
 
 func main() {
+  fmt.Println("Server Runing In localhost",porta)
   http.HandleFunc("/", index)
-  http.ListenAndServe(":8080", nil) 
+  http.ListenAndServe(porta, nil) 
 }
+
 func index(rw http.ResponseWriter, rq *http.Request)  {
-  temp.ExecuteTemplate(rw, "Index", nil)
+  produtos := []Produtos{
+    {Nome: "Camiseta", Descricao: "Azul, Bem Bonita", Preco: 200.00},
+    {"Tenis", "Confortável e bonito", 250.00, 100},
+    {"Fone", "Muito Bom", 70.00, 4},
+  }
+
+  temp.ExecuteTemplate(rw, "Index", produtos)
 }
